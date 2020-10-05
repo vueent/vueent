62f87db0bc6cee69e77ebd108c8a0e90a36a6761
Alexey Nosov <nayretech@gmail.com>
2020-10-06 01:00:15 +0700

    Remove reactive module

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,3 @@
 export { Service, register as registerService, use as useService, inject as injectService } from './service';
-
 export { Controller, register as registerController, use as useController, inject as injectController } from './controller';
-
-export { tracked, calculated } from './reactive';
-
 export { Vueent, useVueent } from './vueent';
