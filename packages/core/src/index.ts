e6903181413c2647565a6b9c39951ca58fbbd91f
Alexey Nosov <nayretech@gmail.com>
2020-10-08 09:48:13 +0700

    Export all from the entrypoint of core package

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,3 +1,19 @@
-export { Service, register as registerService, use as useService, inject as injectService } from './service';
-export { Controller, register as registerController, use as useController, inject as injectController } from './controller';
-export { Vueent, useVueent } from './vueent';
+export {
+  Service,
+  Constructor as ServiceConstructor,
+  Params as ServiceParams,
+  register as registerService,
+  use as useService,
+  inject as injectService
+} from './service';
+
+export {
+  Controller,
+  Constructor as ControllerConstructor,
+  Params as ControllerParams,
+  register as registerController,
+  use as useController,
+  inject as injectController
+} from './controller';
+
+export { Vueent, ServiceRegistry, ControllerRegistry, useVueent } from './vueent';
