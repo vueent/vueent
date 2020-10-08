f14a2f37b814cc68dce34356119f03b81c8f6601
Alexey Nosov <nayretech@gmail.com>
2020-10-08 09:44:17 +0700

    Turn bili back

--- packages/mix-models/fill-dist.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-cp README.md dist && cp LICENSE dist && \
-cat package.json | grep -v '"private":' | grep -v '"fill:dist"' | grep -v '"prepublishOnly"' | grep -v '"prepare"' > dist/package.json
