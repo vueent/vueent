d2a70f2e6590ab7fb7b0f46623980eac9ba2ca45
Alexey Nosov <nayretech@gmail.com>
2020-10-08 08:54:59 +0700

    Update magic again

--- packages/core/fill-dist.sh
+++ packages/core/fill-dist.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 cp README.md dist && cp LICENSE dist && \
-cat package.json | grep -v '"private":' | grep -v '"fill:dist"' | grep -v '"prepublishOnly"' > dist/package.json
+cat package.json | grep -v '"private":' | grep -v '"fill:dist"' | grep -v '"prepublishOnly"' | grep -v '"prepare"' > dist/package.json
