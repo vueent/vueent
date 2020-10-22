d8fbc4880c73dcd2d1d9b837a4dfd5aef6ff4e66
Alexey Nosov <nayretech@gmail.com>
2020-10-22 19:10:17 +0700

    Reuse vue instance between tests

--- packages/mix-models/tests/__mocks__/vue-vm.ts
+++ packages/mix-models/tests/__mocks__/vue-vm.ts
@@ -1,15 +1,19 @@
 import VueCompositionApi from '@vue/composition-api';
 import { Vue, isVue2, createApp, defineComponent } from 'vue-demi';
 
-if (isVue2) Vue.use(VueCompositionApi);
+let VueInstance: unknown | undefined;
 
-const VueInstance: unknown = createApp(
-  defineComponent({
-    name: 'App',
-    render(createElement: Function) {
-      return createElement('div', this.$slots.default);
-    }
-  })
-);
+if (!VueInstance) {
+  if (isVue2) Vue.use(VueCompositionApi);
+
+  VueInstance = createApp(
+    defineComponent({
+      name: 'App',
+      render(createElement: Function) {
+        return createElement('div', this.$slots.default);
+      }
+    })
+  );
+}
 
 export default VueInstance;
