38d8098e9924fc9496b871778a28552e7c5865a1
Daniil Antsupov <antsupov.official@gmail.com>
2020-10-26 13:39:59 +0700

    Update test

--- packages/mix-models/tests/validate/object-validation.test.ts
+++ packages/mix-models/tests/validate/object-validation.test.ts
@@ -22,4 +22,20 @@ test('validation should correctly work with recursive data', () => {
   instance.v.touch();
 
   expect(instance.v.c.internalData.c.internalData.c.id.invalid).toBe(false);
+
+  instance.data.internalData = undefined;
+  instance.data.id = '1';
+  instance.v.touch();
+
+  expect(instance.v.invalid).toBe(false);
+
+  instance.data.internalData = { id: '10', internalData: { id: undefined, internalData: undefined } };
+  instance.v.touch();
+
+  expect(instance.v.c.internalData.c.internalData.invalid).toBe(true);
+  expect(instance.v.invalid).toBe(true);
+
+  if (instance.data.internalData.internalData) instance.data.internalData.internalData.id = '100';
+
+  expect(instance.v.invalid).toBe(false);
 });
