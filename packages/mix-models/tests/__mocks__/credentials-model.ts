d40888bf2804340d368aa985aa8eb7ffade8dac6
Alexey Nosov <nayretech@gmail.com>
2023-01-12 09:39:27 +0700

    33-st Simplify mocks

--- packages/mix-models/tests/__mocks__/credentials-model.ts
+++ packages/mix-models/tests/__mocks__/credentials-model.ts
@@ -30,18 +30,14 @@ export type Validations = PatternAssert<typeof validations, Data>;
 
 export type ModelType = Base<Data> & Rollback & Validate<Validations>;
 
-export interface Model<ModelOptions extends Options> extends DataModel, RollbackPrivate<Data>, ValidatePrivate<Validations> {}
+export interface Model extends DataModel, RollbackPrivate<Data>, ValidatePrivate<Validations> {}
 
-export class Model<ModelOptions extends Options> extends mix<Data, DataModel, typeof DataModel>(
-  DataModel,
-  mixRollback(),
-  mixValidate(validations)
-) {
-  constructor(initialData?: Data, react = true, ...options: ModelOptions[]) {
+export class Model extends mix<Data, DataModel, typeof DataModel>(DataModel, mixRollback(), mixValidate(validations)) {
+  constructor(initialData?: Data, react = true, ...options: Options[]) {
     super('id', initialData ?? { first: '', second: '', last: '' }, react, ...options);
   }
 }
 
-export function create<ModelOptions extends Options>(basicData?: Data, react = true, ...options: ModelOptions[]): ModelType {
+export function create(basicData?: Data, react = true, ...options: Options[]): ModelType {
   return new Model(basicData, react, ...options);
 }
--- packages/mix-models/tests/__mocks__/hard-model.ts
+++ packages/mix-models/tests/__mocks__/hard-model.ts
@@ -74,14 +74,10 @@ export type Validations = PatternAssert<typeof validations, Data>;
 
 export type ModelType = Base<Data> & Rollback & Validate<Validations>;
 
-export interface Model<ModelOptions extends Options> extends DataModel, RollbackPrivate<Data>, ValidatePrivate<Validations> {}
+export interface Model extends DataModel, RollbackPrivate<Data>, ValidatePrivate<Validations> {}
 
-export class Model<ModelOptions extends Options> extends mix<Data, DataModel, typeof DataModel>(
-  DataModel,
-  mixRollback(),
-  mixValidate(validations)
-) {
-  constructor(initialData?: Data, react = true, ...options: ModelOptions[]) {
+export class Model extends mix<Data, DataModel, typeof DataModel>(DataModel, mixRollback(), mixValidate(validations)) {
+  constructor(initialData?: Data, react = true, ...options: Options[]) {
     super(
       'id',
       initialData ?? { id: '', phones: [], credentials: { first: '', second: '', last: '' }, documents: [], items: [] },
